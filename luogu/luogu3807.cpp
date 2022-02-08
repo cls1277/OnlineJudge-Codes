02034249af3799ea471ed0d1f44dae6e8da6d90e
cls1277 <cls1277@163.com>
2022-02-08 20:47:07 +0800

    lucas , combine

---
+++ luogu/luogu3807.cpp
@@ -0,0 +1,63 @@
+//By cls1277
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+#define PI acos(-1)
+#define INF 2147483647
+#define eps 1e-7
+#define Fo(i,a,b) for(LL i=(a); i<=(b); i++)
+#define Ro(i,b,a) for(LL i=(b); i>=(a); i--)
+#define Eo(i,x,_) for(LL i=head[x]; i; i=_[i].next)
+#define Ms(a,b) memset((a),(b),sizeof(a))
+#define lowbit(_) _&(-_)
+#define mk(_,__) make_pair(_,__)
+#define pii pair<int,int>
+#define ls x<<1
+#define rs x<<1|1
+#define endl '\n'
+inline LL read() {
+    LL x = 0, f = 1;char c = getchar();
+    while (!isdigit(c)) { if (c == '-')f = -f;c = getchar(); }
+    while (isdigit(c)) x = (x << 1) + (x << 3) + (c ^ 48ll), c = getchar();
+    return x * f;
+}
+
+const LL maxn = 1e5+5;
+
+LL qpow(LL a, LL b, LL mod) {
+    LL res=1;
+    while(b) {
+        if(b&1)
+            res=res*a%mod;
+        a=a*a%mod;
+        b>>=1;
+    }
+    return res;
+}
+
+LL C(LL n, LL m, LL mod) {
+    LL res=1;
+    Fo(i,1,m)
+        res=res*(n-m+i)%mod*qpow(i,mod-2,mod)%mod;
+    return res;
+}
+
+LL lucas(LL n , LL m , LL mod) {
+    if(!m) return 1;
+    return C(n%mod,m%mod,mod)*lucas(n/mod,m/mod,mod)%mod;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    #ifdef DEBUG
+    freopen("data.txt","r",stdin);
+    #endif
+    LL T; cin>>T;
+    while(T--) {
+        LL n,m,p;
+        cin>>n>>m>>p;
+        n+=m;
+        cout<<lucas(n,m,p)<<endl;
+    }
+    return 0;
+}
