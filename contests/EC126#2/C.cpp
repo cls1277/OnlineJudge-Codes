84e999a9afe236d54066eb8cb41e86c3d7ed8a80
cls1277 <cls1277@163.com>
2022-04-10 11:14:47 +0800

    EC126#2 C

--- contests/EC126#2/C.cpp
+++ contests/EC126#2/C.cpp
@@ -9,7 +9,11 @@ typedef long long LL;
 #define endl '\n'
 
 const LL maxn = 3e5+5;
-LL a[maxn];
+LL a[maxn], b[maxn];
+
+bool cmp (LL x, LL y) {
+	return x>y;
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -24,9 +28,18 @@ int main() {
         Fo(i,1,n) {
             cin>>a[i];
             mx = max(mx, a[i]);
+			b[i] = a[i];
         }
-        Fo(i,1,n) a[i] = mx-a[i];
+		// mx++;
+		LL bmx = mx+1;
+        Fo(i,1,n) {
+			a[i] = mx-a[i];
+			b[i] = bmx-b[i];
+		}
         sort(a+1, a+n+1);
+		sort(b+1, b+n+1);
+		// cout<<a[1];
+		// return 0;
         Fo(i,1,n) {
         	if(!a[i]) continue;
         	LL det = cnt1-cnt2;
@@ -62,7 +75,44 @@ int main() {
 		ans = 2*min(cnt1, cnt2);
 		if(cnt1>cnt2) ans += 2*(cnt1-cnt2)-1;
 		if(cnt1<cnt2) ans += 2*(cnt2-cnt1);
-        cout<<ans<<endl;
+
+		LL res = 0;
+		cnt1=0, cnt2=0;
+		Fo(i,1,n) {
+        	if(!b[i]) continue;
+        	LL det = cnt1-cnt2;
+        	if(det<0) {
+        		LL m = -det;
+        		if(m<=b[i]) {
+        			cnt1 = cnt2;
+        			b[i]-=m;
+				} else {
+					cnt1 += b[i];
+					b[i] = 0;
+				}
+			} else if(det>0) {
+				LL m = det*2;
+				if(m<=b[i]) {
+					cnt2 = cnt1;
+					b[i]-=m;
+				} else {
+					cnt2 += b[i]/2;
+					b[i] %= 2;
+				}
+			}
+			if(!b[i]) continue;
+			LL k = b[i]/3;
+            cnt1 += k;
+            cnt2 += k;
+            b[i] -= k*3;
+            if(!b[i]) continue;
+            else if(b[i]==1) cnt1++;
+            else cnt2++;
+		}
+		res = 2*min(cnt1, cnt2);
+		if(cnt1>cnt2) res += 2*(cnt1-cnt2)-1;
+		if(cnt1<cnt2) res += 2*(cnt2-cnt1);
+        cout<<min(ans, res)<<endl;
     }
     return 0;
 }
